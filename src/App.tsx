abde364a08e1213e1ad199c6960e77efb5d92830
Cacau <37200850+cacaru@users.noreply.github.com>
2025-10-15 23:07:40 +0900

    First Add
    Add first

---
+++ src/App.tsx
@@ -0,0 +1,72 @@
+//import { useState } from 'react'
+
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Layout from "./component/layout";
+import Home from "./routes/home";
+import Profile from "./routes/profile";
+import Login from "./routes/login";
+import CreateAccount from "./routes/create-account";
+import { createGlobalStyle } from "styled-components";
+import reset from "styled-reset";
+import { useEffect, useState } from "react";
+import LoadingScreen from "./component/loading-screen";
+
+const router = createBrowserRouter([
+  {
+    path:"/",
+    element: <Layout />,
+    children: [
+      {
+        path: "",
+        element: <Home />,
+      },
+      {
+        path: "profile",
+        element: <Profile/>,
+      }
+    ]
+  },
+  {
+    path: "/login",
+    element: <Login />
+  },
+  {
+    path: "/create-account",
+    element: <CreateAccount />
+  }
+])
+
+const GlobelStyles = createGlobalStyle`
+  ${reset};
+  * {
+    box-sizing: border-box;
+  }
+  body{
+    background-color: black;
+    color: white;
+  }
+`;
+
+function App() {
+
+  const [isLoading, setLoading] = useState(true);
+  const init = async() => {
+    // wait for firebase
+    setLoading(false);
+  }
+
+  useEffect(()=>{
+    init();
+  }, []);
+
+  return (
+    <>
+      <GlobelStyles />
+      {
+        isLoading ? <LoadingScreen /> : <RouterProvider router={router} />
+      }
+    </>
+  )
+}
+
+export default App
---
+++ src/firebase.tsx
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyA5FY6FkYoKLAlJFvm6dv177nuT0lkphAo",
+  authDomain: "nwitter-reloaded-b7b8f.firebaseapp.com",
+  projectId: "nwitter-reloaded-b7b8f",
+  storageBucket: "nwitter-reloaded-b7b8f.firebasestorage.app",
+  messagingSenderId: "902107989647",
+  appId: "1:902107989647:web:61860416f257a4689dd2fa"
+};
+
+const app = initializeApp(firebaseConfig);
